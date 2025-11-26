a3fc8bab3cb744300fc3cd429724dbb772191014
manolo edge <manoloedge@manolos-MacBook-Pro.local>
2025-11-27 00:06:07 +0100

    Initialize project structure with basic files and configurations

---
+++ bin/openid4vp_dcql.dart
@@ -0,0 +1,5 @@
+import 'package:openid4vp_dcql/openid4vp_dcql.dart' as openid4vp_dcql;
+
+void main(List<String> arguments) {
+  print('Hello world: ${openid4vp_dcql.calculate()}!');
+}
---
+++ test/openid4vp_dcql_test.dart
@@ -0,0 +1,8 @@
+import 'package:openid4vp_dcql/openid4vp_dcql.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
