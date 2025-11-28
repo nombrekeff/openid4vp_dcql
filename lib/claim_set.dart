d542e84e3b39b336f044fe26eadf73cabc7e462f
manolo edge <manoloedge@manolos-MacBook-Pro.local>
2025-11-28 01:59:48 +0100

    feat: Implement ClaimSet class with JsonSerializable and toJson method

--- lib/claim_set.dart
+++ lib/claim_set.dart
@@ -1,5 +1,12 @@
-class ClaimSet {
+import 'package:openid4vp_dcql/json.dart';
+
+class ClaimSet with JsonSerializable {
   List<String> ids;
 
   ClaimSet({required this.ids});
+
+  @override
+  Map<String, dynamic> toJson() {
+    throw UnimplementedError();
+  }
 }
