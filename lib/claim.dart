33983c0e92861a9961607262918b4ee1fe9b7d2f
manolo edge <manoloedge@manolos-MacBook-Pro.local>
2025-12-01 17:48:52 +0100

    style: Reformat long lines in assertions and `toJson` method for improved readability.

--- lib/claim.dart
+++ lib/claim.dart
@@ -9,13 +9,15 @@ class Claim with JsonSerializable {
 
   Claim({this.id, required this.path, this.values}) {
     assert(path.isNotEmpty, 'Claim path cannot be empty');
-    assert(values == null || values!.isNotEmpty, 'Claim values cannot be an empty list');
+    assert(values == null || values!.isNotEmpty,
+        'Claim values cannot be an empty list');
     assert(
       path.every((e) => e is String || e is int || e == null),
       'Claim path elements must be String, int, or null',
     );
     assert(
-      values == null || values!.every((e) => e is String || e is int || e is bool),
+      values == null ||
+          values!.every((e) => e is String || e is int || e is bool),
       'Claim values must be String, int, bool, or null',
     );
   }
@@ -26,7 +28,11 @@ class Claim with JsonSerializable {
 
   @override
   Map<String, dynamic> toJson() {
-    return {if (id != null) 'id': id, 'path': path, if (values != null) 'values': values};
+    return {
+      if (id != null) 'id': id,
+      'path': path,
+      if (values != null) 'values': values
+    };
   }
 
   static Claim fromJson(Map<String, dynamic> claim) {
